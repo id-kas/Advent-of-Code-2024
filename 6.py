7ac86272b94d2e42a4e75df843ee6f25729b598f
CamelotMacaw <91760131+CamelotMacaw@users.noreply.github.com>
2024-12-06 15:29:49 +0100

    actually 6 this time, yesterday was 5

---
+++ 6.py
@@ -0,0 +1,84 @@
+class Position():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        print(f"({self.x},{self.y})")
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+    def to_index(self):
+        return self.y * line_length + self.x
+
+
+
+
+with open("6_input.txt") as file:
+    first_line = file.readline()
+    line_length = len(first_line)
+    content = first_line + file.read()
+
+
+def to_pos(index):
+    return Position(index%line_length, index//line_length)
+
+def is_in_bounds(index):
+    return 0 <= index <= len(content) - 1
+
+
+def move(pos, direction):
+    """ Returns direction that it is heading in after this move """
+    print(direction)
+    if direction == "up":
+        pos.y -= 1
+        if is_in_bounds(pos.to_index()) and content[pos.to_index()] == "#":
+            pos.y += 1
+            pos.x += 1
+            return "right"
+    elif direction == "right":
+        pos.x += 1
+        if is_in_bounds(pos.to_index()) and content[pos.to_index()] == "#":
+            pos.y += 1
+            pos.x -= 1
+            return "down"
+    elif direction == "down":
+        pos.y += 1
+        if is_in_bounds(pos.to_index()) and content[pos.to_index()] == "#":
+            pos.y -= 1
+            pos.x -= 1
+            return "left"
+    elif direction == "left":
+        pos.x -= 1
+        if is_in_bounds(pos.to_index()) and content[pos.to_index()] == "#":
+            pos.y -= 1
+            pos.x += 1
+            return "up"
+    else:
+        print("Programmer error")
+
+    return direction
+
+
+def part_one():
+    positions = set()
+    
+    start_pos = to_pos(content.index("^"))
+
+    direction = "up"
+
+    curr_pos = start_pos
+    while is_in_bounds(curr_pos.to_index()):
+        positions.add(curr_pos)
+        direction = move(curr_pos, direction)
+        
+
+    print("Part one: ", len(positions))
+
+
+part_one()
