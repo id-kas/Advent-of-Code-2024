ce2d127e6d59aaefaee43c90e1c0b9c211804be7
CamelotMacaw <91760131+CamelotMacaw@users.noreply.github.com>
2024-12-08 02:56:02 +0100

    added correct recursive stop condition; I had forgotten to check if the end of the equation had been reached

--- 7.py
+++ 7.py
@@ -19,12 +19,11 @@ equations = []
 with open("7_input.txt") as file:
     for line in file:
         equation = Equation(read_line_cleanly(line))
-        # print(equation)
         equations.append(equation)
 
 
 def is_solveable_rec(equation, current_result, current_index):
-    if current_result == equation.result:
+    if current_result == equation.result and current_index == len(equation.elements):
         return True
     if current_result > equation.result:
         return False
@@ -48,5 +47,35 @@ def part_one():
     print("Part one: ", result)
 
 
+def new_operator(a, b):
+    return int(str(a) + str(b))
+
+
+def is_solveable_rec_2(equation, current_result, current_index):
+    if current_result == equation.result and current_index == len(equation.elements):
+        return True
+    if current_result > equation.result:
+        return False
+    if current_index >= len(equation.elements):
+        return False
+
+    return is_solveable_rec_2(equation, current_result + equation.elements[current_index], current_index+1) or \
+        is_solveable_rec_2(equation, current_result * equation.elements[current_index], current_index+1) or \
+        is_solveable_rec_2(equation, new_operator(current_result, equation.elements[current_index]), current_index+1) 
+
+
+def is_solveable_2(equation):
+    return is_solveable_rec_2(equation, 0, 0)
+
+
+def part_two():
+    result = 0
+    for equation in equations:
+        # print(equation)
+        if is_solveable_2(equation):
+            result += equation.result
+
+    print("Part two: ", result)
 
 part_one()
+part_two()
